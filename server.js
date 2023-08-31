c4d11eee812dd770ea84f39bc0f48a94c5e5318c
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-08-30 21:29:40 -0700

    added express  middleware to server.js file

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+// middleware for parsing different data types
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
