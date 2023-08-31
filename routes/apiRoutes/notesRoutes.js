376f37da10bdf224ea184df5209d142f2bc0580a
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-08-30 23:02:49 -0700

    added post route and imported createNewNote function into notesRoutes.js

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
 let { notesArray } = require('../../db/db.json');
+const { createNewNote } = require('../../public/assets/js/notes');
 
+router.post('/notes', (req, res) => {
+  if(notesArray){
+  req.body.id = notesArray.length.toString();
+  } else 
+  {req.body.id = 0}
+  res.json(createNewNote(req.body, notesArray));
+});
 
 module.exports = router;
