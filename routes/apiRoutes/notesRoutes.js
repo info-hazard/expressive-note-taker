361928aa5add1165a8bf4f6ea47fd34aef0fbf28
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-08-31 01:53:34 -0700

    added get route to notes array in db.json

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 let { notesArray } = require('../../db/db.json');
 const { createNewNote } = require('../../public/assets/js/notes');
 
+router.get('/notes', (req, res) => {
+  let results = notesArray;
+  res.json(results);
+});
+
 router.post('/notes', (req, res) => {
   if(notesArray){
   req.body.id = notesArray.length.toString();
